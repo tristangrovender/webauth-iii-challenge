d6140b268ab5cec02dc617a1acebd0431b4688d5
Tristan Grovender <therealtristangrovender@gmail.com>
2019-05-09 15:25:51 -0500

    users table created and run

---
+++ database/migrations/20190509152359_users.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+      users.increments();
+  
+      users
+        .string('username', 128)
+        .notNullable()
+        .unique();
+      users.string('password', 128).notNullable();
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
+  
+// npx knex init
+// npx knex migrate:make create_roles_table
+// npx knex migrate:latest
